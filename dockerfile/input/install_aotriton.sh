e46e97e4f6682cf66924ce1a5d65b4d0eaa6eed4
Xinya Zhang <Xinya.Zhang@amd.com>
2025-07-15 18:19:53 -0500

    Try to make input/install_aotriton.sh compatitable with -DAOTRITON_TARGET_ARCH and -DTARGET_GPUS

--- dockerfile/input/install_aotriton.sh
+++ dockerfile/input/install_aotriton.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 AOTRITON_GIT_NAME="$1"
-AOTRITON_TARGET_ARCH="$2"
+AOTRITON_TARGETS="$2"
 NOIMAGE_MODE="$3"
 
 cd /root/build
@@ -27,7 +27,12 @@ if [ "$NOIMAGE_MODE" = "OFF" ]; then
 else
   scl enable gcc-toolset-13 "cd aotriton; python -m pip install -r requirements.txt"
 fi
-scl enable gcc-toolset-13 "mkdir -p aotriton/build; cd aotriton/build; cmake .. -DCMAKE_PREFIX_PATH=/opt/rocm -DPYTHON_EXECUTABLE=/usr/bin/python3.11 -DCMAKE_INSTALL_PREFIX=installed_dir/aotriton -DCMAKE_BUILD_TYPE=Release -DAOTRITON_GPU_BUILD_TIMEOUT=0 \"-DAOTRITON_TARGET_ARCH=${AOTRITON_TARGET_ARCH}\" -DAOTRITON_NO_PYTHON=ON -DAOTRITON_NOIMAGE_MODE=${NOIMAGE_MODE} -G Ninja && ninja install/strip"
+OBSOLETE=$(grep -F 'set(TARGET_GPUS "OBSOLETE"' aotriton/CMakeLists.txt|wc -l)
+if [ ${OBSOLETE} -ge 0 ]; then
+  scl enable gcc-toolset-13 "mkdir -p aotriton/build; cd aotriton/build; cmake .. -DCMAKE_PREFIX_PATH=/opt/rocm -DPYTHON_EXECUTABLE=/usr/bin/python3.11 -DCMAKE_INSTALL_PREFIX=installed_dir/aotriton -DCMAKE_BUILD_TYPE=Release -DAOTRITON_GPU_BUILD_TIMEOUT=0 \"-DAOTRITON_TARGET_ARCH=${AOTRITON_TARGETS}\" -DAOTRITON_NO_PYTHON=ON -DAOTRITON_NOIMAGE_MODE=${NOIMAGE_MODE} -G Ninja && ninja install/strip"
+else
+  scl enable gcc-toolset-13 "mkdir -p aotriton/build; cd aotriton/build; cmake .. -DCMAKE_PREFIX_PATH=/opt/rocm -DPYTHON_EXECUTABLE=/usr/bin/python3.11 -DCMAKE_INSTALL_PREFIX=installed_dir/aotriton -DCMAKE_BUILD_TYPE=Release -DAOTRITON_GPU_BUILD_TIMEOUT=0 \"-DTARGET_GPUS=${AOTRITON_TARGETS}\" -DAOTRITON_NO_PYTHON=ON -DAOTRITON_NOIMAGE_MODE=${NOIMAGE_MODE} -G Ninja && ninja install/strip"
+fi
 rocmver=$(scl enable gcc-toolset-13 "cpp -I/opt/rocm/include /input/print_rocm_version.h"|tail -n 1|sed 's/ //g')
 if [ -z ${AOTRITON_TARBALL_SHARD} ]; then
   tarfile=aotriton-${AOTRITON_GIT_NAME}-manylinux_2_28_x86_64-rocm${rocmver}-shared.tar.gz
